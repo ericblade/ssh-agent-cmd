2535c4f069446eae52c3d179f092e24b199d5db0
Eric Blade <blade.eric@gmail.com>
2016-12-10 19:24:40 -0500

    significant updates
        README and file comments updated to hopefully make a lot more sense
        of what this script is doing.
    
        Search %ProgramFiles% and %ProgramFiles(x86)% instead of defaulting
        to drive C
    
        Abort if unable to find any ssh-agent
    
        After finding an existing agent, make sure that the global registry
        is correct, in case it got out of sync somewhere
    
        Unset SSH_AGENT_SEARCHING when the registry is set.

--- README.md
+++ README.md
@@ -4,12 +4,18 @@ You hate entering your password over and over and over when you're using git in 
 Command Processor, right?
 
 Me too.  So, here's a script that will launch ssh-agent, or connect to an existing one.
+
 There's also a registry entry key that you can import, that will cause it to run the script automatically
 in every command processor you open.
 
+You probably DO NOT need to use the auto-start registry hack.  If you place
+this script in your startup folder, it should successfully write the variables
+to your global registry, therefore allowing any further command shells
+that are spawned to know how to use the ssh-agent.
+
 **** WARNINGS ****
 
-YOU MUST EDIT THESE FILES AND PLACE THE CORRECT LOCATIONS TO YOUR FILES, OTHERWISE BAD THINGS MAY HAPPEN.
+IF YOU USE THE REGISTRY HACK TO AUTO-START THIS SCRIPT, YOU MUST EDIT THESE FILES AND PLACE THE CORRECT LOCATIONS TO YOUR FILES, OTHERWISE BAD THINGS MAY HAPPEN.
 
 **** Installation ****
 
@@ -17,7 +23,7 @@ Place sshagent.cmd somewhere within your system.  Edit it to change the SSH_BIN_
 
 If you want to use the registry entry, edit the autorun.reg file, and change the path listed in
 "AutoRun"="d:\\\\sshagent.cmd" to point to the location where your sshagent.cmd you will be using is.
-Then double-click the autorun.reg file. (Make sure you use double slashes in this file! 
+Then double-click the autorun.reg file. (Make sure you use double slashes in this file!
 If you screw up this file, I don't know what will happen. Maybe nothing, maybe you won't be able to
 start a command window again without undoing it)
 
@@ -28,7 +34,16 @@ git config --global credential.helper wincred
 
 **** Caveats ****
 
-Nothing that I know of!
+This will add the following environment variables to your global registry:
+SSH_AGENT_PID (contianing the process id of the ssh-agent.exe instance)
+SSH_AUTH_SOCK (containing the path to the socket file used by ssh-agent)
+
+If you do not already have an environment variable called SSH_BIN_PATH, then that will be added
+for the specific shell that runs this script.
+
+If you use a multi-shell app such as ConsoleZ or Console2 or some such, and you run this script initially
+inside a shell started from that app, then use the "New Tab", the new tab will inherit the old environment,
+as it is actually launched by the app, which still has the old environment.  Open a new window.
 
 **** License ****
 
@@ -36,6 +51,8 @@ This is free.  Plain-old free, public domain. As open as it gets.  Do whatever y
 It'd be super nice, though, if you were to make any changes that someone else would find useful, if you
 contributed those as a pull request.  Open source works best when everyone helps!
 
-Please feel free to discuss this script at http://www.ericbla.de/phpBB3/viewtopic.php?f=6&t=27
+Please feel free to discuss this script at http://www.ericblade.us/phpBB3/viewtopic.php?f=6&t=27
+
+Report problems and send pull requests at http://github.com/ericblade/ssh-agent-cmd
 
 And don't forget: Today, you should BE AWESOME.
--- sshagent.cmd
+++ sshagent.cmd
@@ -1,17 +1,22 @@
 @echo off
+rem -- *** from http://github.com/ericblade/ssh-agent-cmd - free for all use.
+
 rem -- This prevents us from infinite looping at startup.
 rem -- Surprise! "FOR" runs a new command processor for every loop! Wow!
+rem -- So, this is put at the top to short circuit any additional executions that might happen
+rem -- as a side effect of other commands inside.
 IF DEFINED SSH_AGENT_SEARCHING (GOTO :eof)
 set SSH_AGENT_SEARCHING=1
 
-rem -- *** SET THIS PATH TO THE LOCATION WHERE YOUR SSH BINARIES ARE
+rem -- *** SET SSH_BIN_PATH TO THE LOCATION WHERE YOUR SSH BINARIES ARE IF NOT INSTALLED WITH GIT IN
+rem -- *** THE NORMAL PLACE (\program files\ or \program files (x86)\)
 if not defined SSH_BIN_PATH (
     echo Searching for SSH bin path... Define SSH_BIN_PATH to override.
 
-    if exist "c:\program files\git\usr\bin\" (
-        set SSH_BIN_PATH="c:\program files\git\usr\bin\"
-    ) else if exist "c:\program files (x86)\git\usr\bin\" (
-        set SSH_BIN_PATH="c:\program files (x86)\git\usr\bin\"
+    if exist "%ProgramFiles%\git\usr\bin\ssh-agent.exe" (
+        set SSH_BIN_PATH="%ProgramFiles%\git\usr\bin\"
+    ) else if exist "%ProgramFiles(x86)%\git\usr\bin\ssh-agent.exe" (
+        set SSH_BIN_PATH="%ProgramFiles(x86)%\git\usr\bin\"
     )
 
     if defined SSH_BIN_PATH (
@@ -21,27 +26,39 @@ if not defined SSH_BIN_PATH (
     )
 )
 
+IF NOT EXIST %SSH_BIN_PATH%\ssh-agent.exe (
+    echo "*** Unable to find ssh-agent.exe in %ProgramFiles%\git\usr\bin or %ProgramFiles(x86)%\git\usr\bin or %SSH_BIN_PATH%\. Check SSH_BIN_PATH."
+    exit /b
+)
+
 rem -- NOTE: If you kill an agent, the socket file remains locked by Windows! Bad!
 rem -- This means you'll need to change the below filename if you want to run the
 rem -- script again without rebooting.
+rem -- ** NEW NOTE: This no longer seems to be the case in Windows 10 AU. Not sure when
+rem -- exactly that changed between 8.0 and 10AU, but it's a welcome fix!
+
 set SSH_AUTH_SOCK=%TEMP%\ssh-agent-socket.tmp
 
 :checkAgent
 SET "SSH_AGENT_PID="
-rem -- Call cmd /c to find it, because Take Command's "tasklist" is NOT format compatible with CMD.exe!!
+rem -- Get a list of all running tasks, and search it for ssh-agent.exe, record that process id in
+rem -- SSH_AGENT_PID.
+rem -- Call cmd /c to get a known working tasklist command, because Take Command's "tasklist" is NOT format compatible with CMD.exe!!
 FOR /F "tokens=1-2" %%A IN ('cmd /c tasklist^|find /i "ssh-agent.exe"') DO @(IF %%A==ssh-agent.exe (call :agentexists %%B))
+rem -- If no SSH_AGENT_PID found, then start a new instance of ssh-agent.exe
 IF NOT DEFINED SSH_AGENT_PID (GOTO :startagent)
 CALL :setregistry
-set SSH_AGENT_SEARCHING=
 GOTO :eof
 
 :doAdds
+ rem -- Grab all the *_rsa.* files in %USERPROFILE% and add them to the agent, this will probably
+ rem -- prompt you for passwords to those keys.
  FOR /R %USERPROFILE%\.ssh\ %%A in (*_rsa.) DO %SSH_BIN_PATH%\ssh-add %%A >nul 2>&1
  EXIT /b
 
 :agentexists
  SET SSH_AGENT_PID=%1
- EXIT /b
+ GOTO :setregistry
 
 :startagent
  rem -- win 8.1 at least has these set as system, so you can't delete them
@@ -49,18 +66,20 @@ GOTO :eof
  del /f /q %SSH_AUTH_SOCK% >nul 2>&1
  %SSH_BIN_PATH%\ssh-agent -a %SSH_AUTH_SOCK% >nul 2>&1
  CALL :doAdds
+ rem -- recheck the agent to make sure that it launched properly.
  rem -- Yes, I know this could cause an infinite loop if it can't find one and can't start one.
- rem -- I can't seem to figure out how to prevent that.
+ rem -- If you find a common cause for this to be a problem, file a bug, or submit a pull
+ rem -- request to fix it :-)
  GOTO :checkAgent
 
 :setregistry
- rem -- store these in the registry. We still need to actually search for the process and
- rem -- such at startup, in case the process has died, we rebooted, or what not, but this
- rem -- should allow non-CMD command parsers such as bash, Take Command, PowerShell, etc
- rem -- or if you're not using the autorun registry change, to pick up the environment.
- rem -- Note that SetX does not affect any already open command shells.
+ rem -- store these variables to the global environment -- note that this can and will NOT affect
+ rem -- any open shells or other running programs.  This also includes affecting new shells opened
+ rem -- by already running programs, such as using the "New Tab" button in ConsoleZ.  This will
+ rem -- only take affect automatically in completely new processes.
  SetX SSH_AUTH_SOCK %SSH_AUTH_SOCK% >nul 2>&1
  SetX SSH_AGENT_PID %SSH_AGENT_PID% >nul 2>&1
+ set SSH_AGENT_SEARCHING=
  EXIT /b
 
 :eof
